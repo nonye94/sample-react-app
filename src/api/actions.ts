b7368e519383e203564f97520ae69479c3114bc5
nonye94 <149767618+nonye94@users.noreply.github.com>
2025-03-02 17:23:55 +0000

    local host changed to 3000

--- src/api/actions.ts
+++ src/api/actions.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosError } from "axios";
 
-const API_URL  = "http://localhost:5000";
+const API_URL  = "http://localhost:3000";
 
 export const getWeatherData = async (city: string): Promise<WeatherData> => {
   return new Promise<WeatherData>((resolve, reject) => {
